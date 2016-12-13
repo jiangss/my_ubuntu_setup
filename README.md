701014d8eb30b7144d2fe158f68f6830d908ed3f
frost <ssjiang09@gmail.com>
2016-12-12 22:13:10 -0800

    packages to install

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 ## Pre-installation:
-* sublime
-* install git
+* $ sudo apt-get update
+* $ sudo apt-get install -y zsh
+* $ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+* Install sublime3
+* $ sudo apt-get update; sudo apt-get install -y git
+* $ ssh-keygen -t rsa
+* Add key to github
+* Clone the setup repo
+* # Install docker as follows:
+* $ sudo apt-get install apt-transport-https ca-certificates gnupg2
+* $ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+* # add `/deb https://apt.dockerproject.org/repo debian-wheezy main` to file `/etc/apt/sources.list.d/docker.list`
+* $ sudo apt-get update
 
 ## Install Packages
+* $ ./batch_install.sh
+
+## Post-installations:
+* Install IntelliJ
---
+++ batch_install.sh
@@ -0,0 +1,36 @@
+sudo apt-get update
+
+# install vlc
+sudo apt-get install -y vlc browser-plugin-vlc
+
+# install basic tools
+sudo apt-get install -y perl net-tools
+sudo apt-get install -y make gcc kernel-devel
+sudo apt-get install -y vim nano wget curl lsof dstat iotop dnsmasq mtr mlocate shim
+sudo apt-get install -y tmux openssl grep htop
+
+# install python packages
+sudo apt-get install -y python-setuptools python-dev build-essential
+sudo easy_install pip
+sudo pip install scipy numpy nose ipython pandas scikit-learn nltk matplotlib patsy statsmodels ggplot
+
+# install docker
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y docker-engine
+sudo service docker start
+# sudo docker run hello-world
+
+# install java JDK
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install -y oracle-java8-set-default
+
+# install sbt
+wget http://apt.typesafe.com/repo-deb-build-0002.deb
+sudo dpkg -i repo-deb-build-0002.deb
+sudo apt-get update
+sudo apt-get install -y sbt scala
+
+
+
